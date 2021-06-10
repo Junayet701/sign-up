63805607f257af0c140f1993162f973acdfa5b43
junayet 701 <junayetahmed49@gmail.com>
2021-06-10 15:57:20 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var a= prompt('enter the number');
+var b= prompt('enter the second number');
+if(a<b){
+    document.write('a is big');
+}
+else if(b>a){
+    document.write('b is big');
+}
+else{
+    document.write('something else');
+}
